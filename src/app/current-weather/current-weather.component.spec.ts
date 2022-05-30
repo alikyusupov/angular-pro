cab96cd8942e3746c63444d8eb063afc13d5b1a4
alikyusupov <kleot@mail.ru>
2022-05-30 17:04:54 +0300

    Chapter 04 (#1)
    * unit tests
    
    * update variables
    
    * add .circleci folder
    
    * update config.yml
    
    * fix package.json
    
    Co-authored-by: a.iusupov <kleot@mail>

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,25 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser'
+import { of } from 'rxjs';
 
+import { WeatherService } from '../weather/weather.service';
+import { fakeWeather } from '../weather/weather.service.fake'
 import { CurrentWeatherComponent } from './current-weather.component';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj(WeatherService, ['getCurrentWeather'])
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+      declarations: [CurrentWeatherComponent],
+      providers: [{ provide: WeatherService, useValue: weatherServiceSpy }]
     })
-    .compileComponents();
+      .compileComponents();
+    weatherServiceMock = TestBed.inject(WeatherService) as any
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+
+    expect(weatherServiceMock.getCurrentWeather)
+      .toHaveBeenCalledTimes(1)
   });
+
+  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather
+      .and.returnValue(of(fakeWeather))
+    // Act
+    fixture.detectChanges() // triggers ngOnInit()
+    // Assert
+    expect(component.current).toBeDefined()
+    expect(component.current.city).toEqual('Bethesda')
+    // Assert on DOM
+    const debugEl = fixture.debugElement
+    const titleEl: HTMLElement = debugEl.query(By.css('span'))
+      .nativeElement
+    expect(titleEl.textContent).toContain('Bethesda')
+  })
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appId: "dec2936e9e6cb83abf1a0e7878f6022e",
+  baseUrl: 'https://',
 }
